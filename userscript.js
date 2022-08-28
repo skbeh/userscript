ae88843866e41ea457fb1b63ebc78a21cc24ba13
Akatsuki Rui <3736910+akiirui@users.noreply.github.com>
2022-08-29 00:59:12 +0800

    feat: redirect to nga.178.com
    Because bbs.nga.cn temporarily unavailable.

--- userscript.js
+++ userscript.js
@@ -6,12 +6,12 @@
 // @description:en-US   Automatically redirect NGA domains to bbs.nga.cn
 // @description:zh-CN   自动重定向 NGA 域名到 bbs.nga.cn
 // @namespace           nga-redirector
-// @version             2022.02.10
+// @version             2022.08.28
 // @author              Akatsuki Rui
 // @license             MIT License
 // @run-at              document-start
+// @match               *://bbs.nga.cn/*
 // @match               *://g.nga.cn/*
-// @match               *://nga.178.com/*
 // @match               *://ngabbs.com/*
 // @match               *://ngacn.cc/*
 // @match               *://yues.org/*
@@ -19,4 +19,6 @@
 
 "use strict";
 
-window.location.replace(location.href.replace(location.hostname, "bbs.nga.cn"));
+window.location.replace(
+  location.href.replace(location.hostname, "nga.178.com")
+);
